af6dc2110af424c52c5734d41b1e27d979b217f8
Sarah Morris <sarah.morris9@gmail.com>
2020-05-04 11:36:34 -0500

    Starting maps project

--- interfaces.ts
+++ interfaces.ts
@@ -1,19 +1,18 @@
-interface Vehicle {
-  name: string,
-  year: number,
-  broken: boolean
+interface Reportable {
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
   year: 2000,
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
-const printVehicle = (vehicle: Vehicle) => {
-  console.log(`Name: ${vehicle.name}`)
-  console.log(`Year: ${vehicle.year}`)
-  console.log(`Broken? ${vehicle.broken}`)
+const printSummary = (item: Reportable) => {
+  console.log(item.summary());
 };
 
-printVehicle(oldCivic)
+printSummary(oldCivic);
