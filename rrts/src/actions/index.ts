494f53acbdd147942e13683ccab3a8808342cdad
Sarah Morris <sarah.morris9@gmail.com>
2020-05-07 14:02:56 -0500

    Finished react portion

--- rrts/src/actions/index.ts
+++ rrts/src/actions/index.ts
@@ -1,22 +1,2 @@
-import axios from 'axios';
-import { Dispatch } from 'redux';
-import { ActionTypes } from "./types";
-
-const url = 'http://jsonplaceholder.typicode.com/todos';
-
-export const fetchTodos = () => {
-  return async (dispatch: Dispatch) => {
-    const response = await axios.get<Todo[]>(url);
-
-    interface Todo {
-      id: number;
-      title: string;
-      completed: boolean;
-    }
-
-    dispatch({
-      type: ActionTypes.fetchTodos,
-      payload: response.data,
-    });
-  };
-};
+export * from './todos';
+export * from './types';
